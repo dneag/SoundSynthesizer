9db7074c0ed78a0c6de0faa453c074faa0e8bca2
Dan Neag <dneag@hotmail.com>
2021-04-11 19:14:40 -0400

    Copy some code to main()

--- SoundSynthesizer/main.cpp
+++ SoundSynthesizer/main.cpp
@@ -3,8 +3,21 @@
 */
 
 #include <iostream>
+#include <vector>
+
+#include "noiseMaker.h"
 
 int main()
 {
+	// get all sound hardware
+	std::vector<std::wstring> devices = olcNoiseMaker<short>::Enumerate();
+
+	// display findings
+	for (auto d : devices)
+		std::wcout << "Found Output Device:" << d << std::endl;
+
+	// create sound machine
+	olcNoiseMaker<short> sound(devices[0], 44100, 1, 8, 512);
+
 	return 0;
 } 
