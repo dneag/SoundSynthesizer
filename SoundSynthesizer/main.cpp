f20373e01b3cbe484f914bb2e4c352513a0ff562
Dan Neag <dneag@hotmail.com>
2021-04-11 19:15:19 -0400

    Add makeNoise()

--- SoundSynthesizer/main.cpp
+++ SoundSynthesizer/main.cpp
@@ -7,6 +7,14 @@
 
 #include "noiseMaker.h"
 
+// dTime represents the time passed since the start of the program
+double makeNoise(double dTime) {
+	
+	// amplitude (0.5) times sin of frequency (440) times dTime 
+	// the 2 * pi converts the hertz to angular velocity (required because sin doesn't 'understand' hertz)
+	return 0.5 * sin(440.0 * 2. * 3.14159 * dTime);
+}
+
 int main()
 {
 	// get all sound hardware
