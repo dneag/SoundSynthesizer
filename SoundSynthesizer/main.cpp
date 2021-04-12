57364480bfc68e1711daf1bfe1d537bd401c1cdf
Dan Neag <dneag@hotmail.com>
2021-04-12 18:21:09 -0400

    Revert

--- SoundSynthesizer/main.cpp
+++ SoundSynthesizer/main.cpp
@@ -7,22 +7,12 @@
 
 #include "noiseMaker.h"
 
-atomic<double> dFrequencyOutput = 0.;
-
 // dTime represents the time passed since the start of the program
 double makeNoise(double dTime) {
 	
-	// amplitude (0.5) times sin of frequency (440 - an A) times dTime 
+	// amplitude (0.5) times sin of frequency (440) times dTime 
 	// the 2 * pi converts the hertz to angular velocity (required because sin doesn't 'understand' hertz)
-	double dOutput = sin(dFrequencyOutput * 2. * 3.14159 * dTime);
-
-	// makes a square wave
-	/*if (dOutput > 0.)
-		return 0.008;
-	else
-		return -0.008;*/
-
-	return dOutput * 0.5; // adjust volume by multiplying
+	return 0.5 * sin(440.0 * 2. * 3.14159 * dTime);
 }
 
 int main()
@@ -36,31 +26,6 @@ int main()
 
 	// create sound machine
 	olcNoiseMaker<short> sound(devices[0], 44100, 1, 8, 512);
-
-	// link noise function with sound machine
-	sound.SetUserFunction(makeNoise);
-
-	double dOctaveBaseFrequency = 110.0; // A2
-	double d12thRootOf2 = pow(2., 1. / 12.);
-
-	while (true) {
-
-		bool bKeyPressed = false;
-		for (int k = 0; k < 15; k++) {
-			
-			if (GetAsyncKeyState((unsigned char)("ZSXCFVGBNJMK\xbcL\xbe"[k])) && 0x8000) {
-
-				std::cout << dOctaveBaseFrequency * pow(d12thRootOf2, k) << std::endl; // displays current frequency on keypress
-				dFrequencyOutput = dOctaveBaseFrequency * pow(d12thRootOf2, k);
-				bKeyPressed = true;
-			}
-		}
-		
-		if (!bKeyPressed) {
-
-			dFrequencyOutput = 0.;
-		}
-	}
 
 	return 0;
 } 
